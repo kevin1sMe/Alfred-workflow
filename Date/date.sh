f707829c17a58d65cbc022564fe0eaccc3f08b02
kevinlin <linjiang1205@qq.com>
2017-11-13 13:49:30 +0800

    Add option now

--- Date/date.sh
+++ Date/date.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 
-echo {query} | egrep "^[0-9]{9,10}$"
+query={query}
+result=""
+
+echo ${query} | egrep "^[0-9]{9,10}$"
 if [ $? -eq 0 ]
 then
-    result=`date -r "{query}" "+%Y-%m-%d %H:%M:%S"`
+    result=`date -r "${query}" "+%Y-%m-%d %H:%M:%S"`
 else
-    result=`date -j -f "%Y-%m-%d %H:%M:%S" "{query}" +%s`
+    if [ ${query} == "now"  ]
+    then
+        result=`date +%s`
+    else
+        result=`date -j -f "%Y-%m-%d %H:%M:%S" "${query}" +%s`
+    fi
 fi
 
 echo "<?xml version=\"1.0\"?>"
